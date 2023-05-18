00b7a5b39fb4b210c2bc4fe982a38820315ccf5a
Otavio Andrade <otavio.sto@gmail.com>
2023-05-17 21:08:07 -0300

    fix: calculo de retangulo orientado a objetos

--- CalculoRetangulo/src/Programa.java
+++ CalculoRetangulo/src/Programa.java
@@ -1,3 +1,7 @@
+/*Crie uma classe "Retângulo" que
+ tenha como atributos a altura,
+  a largura, a cor e o material do retângulo,
+   e como métodos a área e o perímetro.*/
 public class Programa {
     public static void main(String[] args) {
         Retangulo retanguloUm = new Retangulo();
