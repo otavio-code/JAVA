9f68173e7a86f86095e945276a97bb0da547cae4
Otavio Andrade <otavio.sto@gmail.com>
2023-05-18 20:36:07 -0300

    update

--- Animal/src/Animal.java
+++ Animal/src/Animal.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Animal {
+public class Animal {
+    String nome;
+    int idade;
+    String especie;
+    String habitat;
+
+    String som;
+
+    public String Som(String som){
+        this.som = som;
+        return ("Este animal faz: " + som);
+    }
 }
---
+++ Animal/src/Programa.java
@@ -0,0 +1,19 @@
+public class Programa {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+
+        animal.nome = "Bidu";
+        animal.especie = "Cachorro";
+        animal.habitat = "Do lar";
+        animal.idade = 4;
+        String som;
+        som = "AuAuAuAu";
+
+        System.out.println("Nome: " + animal.nome);
+        System.out.println("Espécie: " + animal.especie);
+        System.out.println("Habitat: " + animal.habitat);
+        System.out.println("Idade: " + animal.idade);
+        System.out.println(animal.Som(som));
+
+    }
+}
